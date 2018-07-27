4da8f403452103ce32f3dd7b971e31ad0600d03d
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-27 00:00:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|power| power.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|check| check > 4}
 end
 
 def find_the_cheese# code an argument here
