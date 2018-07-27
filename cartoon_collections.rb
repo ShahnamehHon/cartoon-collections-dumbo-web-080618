2b33347f5576456ce390b28317589b3244c0a95c
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-27 00:11:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? {|check| check.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
